0497ae844d948786e6e28d776f4df264245c642b
Slav1Bak <players7@interia.pl>
2019-02-14 10:47:45 +0100

    java

---
+++ src/start/Hello.java
@@ -0,0 +1,9 @@
+package start;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(" JRE Version: " + System.getProperty("java.runtime.version"));
+    }
+}
