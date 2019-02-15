5827d69b61a2256800d71857273817f8e1df751c
Slav1Bak <players7@interia.pl>
2019-02-15 09:46:28 +0100

    Wszystko z dnia pierwszego

--- src/scaner/Skanowanie.java
+++ src/scaner/Skanowanie.java
@@ -1,18 +1,29 @@
 package scaner;
 
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Skanowanie {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj wartosc a");
+       /* System.out.println("podaj wartosc a");
         int a= scanner.nextInt();
         System.out.println("podaj wartosc b");
         int b= scanner.nextInt();
 
         System.out.println(a+b);
-
+*/
+        System.out.println("podaj staz pracy");
+        scanner.useLocale(Locale.US);
+        double staz = scanner.nextDouble();
+        System.out.println("Twoj staz to " + staz);
+//      trzeba dodac linie
+        scanner.nextLine();
+        System.out.println("podaj imie");
+        String name = scanner.nextLine();
+        System.out.println("twoje imie " + name);
+        scanner.close();
     }
 }
